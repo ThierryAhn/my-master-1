f5d93e16f746fb63dd07d102a0a61e225a89db46
folabi41 <folabi41@a300b035-8e1f-f0f4-2205-723fad1b520f>
2013-04-16 12:19:55 +0000

    Ajout du tp

---
+++ semestre2/architectures_distribues/TP3_4/src/main/java/tp/model/CityManagerService.java
@@ -0,0 +1,15 @@
+package tp.model;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+@WebService
+public interface CityManagerService {
+	boolean addCity(City city);
+	boolean removeCity(City city);
+	List<City> getCities();
+	City searchExactPosition(Position position) throws CityNotFound;
+	List<City> searchNear(Position position);
+	List<City> searchFor(String cityName);
+}
